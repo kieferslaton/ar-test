1d94febe4b2226e6ded4d10d763cf10d852f0075
Kiefer Slaton <kslaton@madg.com>
2021-10-14 14:32:56 -0500

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c05f51cee5ec4df212aca686f89698d321c2e978',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c05f51cee5ec4df212aca686f89698d321c2e978',
+    ),
+  ),
+);
